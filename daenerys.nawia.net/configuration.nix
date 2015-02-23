e2c860e9b03489e1efc0c1a8ab76eb834d7c0ecd
Mariusz `shd` Gliwiński <shd@nawia.net>
2015-02-23 21:18:05 +0100

    daenerys.nawia.net update

--- daenerys.nawia.net/configuration.nix
+++ daenerys.nawia.net/configuration.nix
@@ -1,26 +1,26 @@
 { config, pkgs, ... }:
 
 {
-  imports = [
-    ./hardware-configuration.nix
-  ];
+	imports = [
+		./hardware-configuration.nix
+	];
 
-  boot = {
-    cleanTmpDir = true;
-    loader = {
-      grub = {
-        enable = true;
-        version = 2;
-        device = "/dev/sda";
-      };
-    };
-  };
+	boot = {
+		cleanTmpDir = true;
+		loader = {
+			grub = {
+				enable = true;
+				version = 2;
+				device = "/dev/sda";
+			};
+		};
+	};
 
-  fileSystems."/" = {
-    device = "/dev/disk/by-label/nixos";
-    fsType = "ext4";
-  };
-	
+	fileSystems."/" = {
+		device = "/dev/disk/by-label/nixos";
+		fsType = "ext4";
+	};
+
 	security.sudo = {
 		enable = true;
 		wheelNeedsPassword = false;
@@ -28,14 +28,14 @@
 
 	networking.hostName = "daenerys";
 
-  i18n =
-  {
-    consoleFont = "lat2-16";
-    consoleKeyMap = "pl";
-    defaultLocale = "pl_PL.UTF-8";
-  };
+	i18n =
+	{
+		consoleFont = "lat2-16";
+		consoleKeyMap = "pl";
+		defaultLocale = "pl_PL.UTF-8";
+	};
 
-  time.timeZone = "Europe/Warsaw";
+	time.timeZone = "Europe/Warsaw";
 
 	services = {
 		ntp = {
@@ -47,11 +47,11 @@
 		};
 	};
 
-  users.extraUsers.shd = {
-    extraGroups = [ "wheel" ];
-    isNormalUser = true;
-    openssh.authorizedKeys.keys = [ "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAIEAmNhcSbjZB3BazDbmmtqPCDzVd+GQBJI8WAoZNFkveBGC0zznUCdd78rrjke5sDRBVCIqKABCx5iwU4VM1zVWZfWlsf6HEbhyUVdWmKgylG7Mchg2dkJUfTHx/VLnE1gDqc1+9SSs88q6H+IO4Kex853Q7eUo9Cmsi8TUn9rthME=" ];
-  };
+	users.extraUsers.shd = {
+		extraGroups = [ "wheel" ];
+		isNormalUser = true;
+		openssh.authorizedKeys.keys = [ "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAIEAmNhcSbjZB3BazDbmmtqPCDzVd+GQBJI8WAoZNFkveBGC0zznUCdd78rrjke5sDRBVCIqKABCx5iwU4VM1zVWZfWlsf6HEbhyUVdWmKgylG7Mchg2dkJUfTHx/VLnE1gDqc1+9SSs88q6H+IO4Kex853Q7eUo9Cmsi8TUn9rthME=" ];
+	};
 
 	environment.systemPackages = with pkgs;
 	[
