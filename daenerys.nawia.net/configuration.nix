7efdd7a68ab169f9fcb79087cc5d613fd01c7a38
Mariusz `shd` Gliwiński <alienballance@gmail.com>
2015-02-23 10:01:53 -0100

    daenery.nawia.net init

---
+++ daenerys.nawia.net/configuration.nix
@@ -0,0 +1,60 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+  ];
+
+  boot = {
+    cleanTmpDir = true;
+    loader = {
+      grub = {
+        enable = true;
+        version = 2;
+        device = "/dev/sda";
+      };
+    };
+  };
+
+  fileSystems."/" = {
+    device = "/dev/disk/by-label/nixos";
+    fsType = "ext4";
+  };
+	
+	security.sudo = {
+		enable = true;
+		wheelNeedsPassword = false;
+	};
+
+	networking.hostName = "daenerys";
+
+  i18n =
+  {
+    consoleFont = "lat2-16";
+    consoleKeyMap = "pl";
+    defaultLocale = "pl_PL.UTF-8";
+  };
+
+  time.timeZone = "Europe/Warsaw";
+
+	services = {
+		ntp = {
+			enable = true;
+			servers = [ "0.pl.pool.ntp.org" "1.pl.pool.ntp.org" "2.pl.pool.ntp.org" "3.pl.pool.ntp.org" ];
+		};
+		openssh = {
+			enable = true;
+		};
+	};
+
+  users.extraUsers.shd = {
+    isNormalUser = true;
+    openssh.authorizedKeys.keys = [ "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAIEAmNhcSbjZB3BazDbmmtqPCDzVd+GQBJI8WAoZNFkveBGC0zznUCdd78rrjke5sDRBVCIqKABCx5iwU4VM1zVWZfWlsf6HEbhyUVdWmKgylG7Mchg2dkJUfTHx/VLnE1gDqc1+9SSs88q6H+IO4Kex853Q7eUo9Cmsi8TUn9rthME=" ];
+  };
+
+	environment.systemPackages = with pkgs;
+	[
+		vim
+	];
+
+}
