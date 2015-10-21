51b8c711c8647763607979f4f193c4c4968ae9a3
Mariusz `shd` Gliwiński <alienballance@gmail.com>
2015-10-21 03:45:48 +0200

    [common] user/shd [workstation] fonts,hardware

--- config/common.nix
+++ config/common.nix
@@ -7,6 +7,11 @@ in
 with lib;
 
 {
+  imports = [
+    ../user/default.nix
+  ];
+  user.shd.enable = true;
+
 	boot.cleanTmpDir = true;
 	services = {
 		ntp = {
@@ -40,15 +45,6 @@ with lib;
 			
 			nmap wireshark curl aria2 socat
 		];
-	};
-	users.extraUsers = {
-		shd = {
-			extraGroups = [ "wheel" ];
-			isNormalUser = true;
-			openssh.authorizedKeys.keys = [
-				"ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAIEAmNhcSbjZB3BazDbmmtqPCDzVd+GQBJI8WAoZNFkveBGC0zznUCdd78rrjke5sDRBVCIqKABCx5iwU4VM1zVWZfWlsf6HEbhyUVdWmKgylG7Mchg2dkJUfTHx/VLnE1gDqc1+9SSs88q6H+IO4Kex853Q7eUo9Cmsi8TUn9rthME="
-			];
-		};
 	};
 	nix.gc = {
 		automatic = true;
