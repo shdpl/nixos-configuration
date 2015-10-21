f122f67055fdbaa9e269aaf449bc4c53d312b25b
Mariusz `shd` Gliwiński <alienballance@gmail.com>
2015-10-21 13:54:16 +0200

    [daenerys] separate web server

---
+++ config/web-server.nix
@@ -0,0 +1,35 @@
+{ config, pkgs, ... }:
+
+{
+  networking.firewall.allowedTCPPorts = [ 80 443 ];
+  nginx = {
+    enable = true;
+    httpConfig = ''
+      server {
+        listen 443 ssl;
+        server_name www.nawia.net;
+
+        ssl_protocols TLSv1 TLSv1.1 TLSv1.2;
+        ssl_certificate ${../private/mail.nawia.net.crt};
+        ssl_certificate_key ${../private/mail.nawia.net.key};
+        ssl_client_certificate ${../private/nawia.net.pem};
+        ssl_verify_client on;
+
+        root /var/www;
+        location /dl {
+          autoindex on;
+        }
+        location /syncthing/ {
+          proxy_set_header Host $host;
+          proxy_set_header X-Real-IP $remote_addr;
+          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+          proxy_pass http://localhost:8384/;
+        }
+        location /deluge {
+          proxy_pass http://localhost:8112/;
+          proxy_set_header X-Deluge-Base "/deluge/";
+        }
+      }
+    '';
+  };
+}
--- hosts/daenerys/configuration.nix
+++ hosts/daenerys/configuration.nix
@@ -9,6 +9,7 @@
 		../../config/ssh.nix
 		../../config/common.nix
 		../../config/mail-server.nix
+    ../../config/web-server.nix
 	];
 
 	networking = {
@@ -17,7 +18,7 @@
 #		tcpcrypt.enable = true;
 		firewall = {
 			enable = true;
-			allowedTCPPorts = [ 53 80 443 22000 ];
+			allowedTCPPorts = [ 53 22000 ];
 			allowedUDPPorts = [ 53 ];
 			allowedTCPPortRanges = [
 				{ from = 8000; to = 8100; }
@@ -29,36 +30,6 @@
 	};
 
 	services = {
-		nginx = {
-			enable = true;
-			httpConfig = ''
-				server {
-					listen 443 ssl;
-					server_name www.nawia.net;
-
-					ssl_protocols TLSv1 TLSv1.1 TLSv1.2;
-					ssl_certificate ${../../private/mail.nawia.net.crt};
-					ssl_certificate_key ${../../private/mail.nawia.net.key};
-					ssl_client_certificate ${../../private/nawia.net.pem};
-					ssl_verify_client on;
-
-					root /var/www;
-					location /dl {
-						autoindex on;
-					}
-					location /syncthing/ {
-						proxy_set_header Host $host;
-						proxy_set_header X-Real-IP $remote_addr;
-						proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-						proxy_pass http://localhost:8384/;
-					}
-					location /deluge {
-						proxy_pass http://localhost:8112/;
-						proxy_set_header X-Deluge-Base "/deluge/";
-					}
-				}
-			'';
-		};
 		tor = {
 			enable = true;
 			relay = {
