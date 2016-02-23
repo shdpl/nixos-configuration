e8f5f0ff5f3fa3437ba089141ef24bc4b778f807
Mariusz 'shd' Gliwiński <shd@nawia.net>
2016-02-23 18:07:21 +0100

    [caroline] init

--- network/nawia/hosts.nix
+++ network/nawia/hosts.nix
@@ -1,5 +1,6 @@
 {
 	daenerys = import ../../hosts/daenerys.nix;
+	caroline = import ../../hosts/caroline.nix;
 #  joan = import ../../hosts/joan.nix;
 #  livewyer = import ../../hosts/livewyer.nix;
 #  magdalene = import ../../hosts/magdalene.nix;
--- network/nawia/machines.nix
+++ network/nawia/machines.nix
@@ -1,5 +1,6 @@
 {
 	daenerys = import ../../machines/daenerys.nix;
+	caroline = import ../../machines/caroline.nix;
 #  joan = import ../../machines/joan.nix;
 #  livewyer = import ../../hosts/livewyer.nix;
 #  magdalene = import ../../hosts/magdalene.nix;
