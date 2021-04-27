efdecd2ee448ec27a5c4061255cfbc04648479cf
Mariusz `shd` Gliwiński <shd@nawia.net>
2021-04-27 18:38:55 +0200

    [i3] move to different workspace with [ and ], [escola] ulearn cert

--- modules/work/escola.nix
+++ modules/work/escola.nix
@@ -19,4 +19,14 @@ in
     ".ssh/escola_bitbucket".source = ../../private/ssh/escola_bitbucket;
     ".ssh/escola_bitbucket.pub".source = ../../data/ssh/escola_bitbucket.pub;
   };
+  # services.caddy = {
+  #   enable = true;
+  #   email = "shd@nawia.net";
+  #   config =
+  #     ''
+  #       ulearn.nawia.net {
+  #         reverse_proxy 127.0.0.1:1000
+  #       }
+  #     '';
+  # };
 }
