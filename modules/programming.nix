d4a7b6b48d02fe4be896d4ca39d2cfbbd214d984
Mariusz `shd` Gliwiński <shd@nawia.net>
2025-11-09 12:11:55 +0100

    [modules/programming] enable postgis for prisma database

--- modules/programming.nix
+++ modules/programming.nix
@@ -428,6 +428,20 @@ with lib;
         '';
         ensureDatabases = [ "prisma" ];
         ensureUsers = [ { name = "prisma"; ensureDBOwnership = true; ensureClauses = { login = true; }; } ];
+        extensions = ps: with ps; [
+          postgis
+        ];
+      };
+      systemd.services."postgresql-prisma-postgis-enabled" = {
+        serviceConfig.Type = "oneshot";
+        wantedBy = [ "multi-user.target" ];
+        after = [ "postgresql.service" ];
+        serviceConfig.User = "postgres";
+        environment.PSQL = "psql --port=${toString config.services.postgresql.settings.port}";
+        path = [ pkgs.postgresql ];
+        script = ''
+          $PSQL prisma -c 'CREATE EXTENSION IF NOT EXISTS "postgis"'
+        '';
       };
     })
     (mkIf (cfg.enable == true && cfg.typescript == true) {
